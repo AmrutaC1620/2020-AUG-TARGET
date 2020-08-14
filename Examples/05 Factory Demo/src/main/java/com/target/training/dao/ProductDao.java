06b06c9f508cea986eb583dee90bf21170e0671a
Vinod Kumar Kayartaya <vinod@vinod.co>
2020-08-14 10:57:54 +0530

    🇮🇳

---
+++ Examples/05 Factory Demo/src/main/java/com/target/training/dao/ProductDao.java
@@ -0,0 +1,10 @@
+package com.target.training.dao;
+
+public interface ProductDao {
+    // CRUD OPERATIONS (TBD LATER)
+
+
+    // QUERIES (more query functions later)
+
+    public int count() throws DaoException;
+}
---
+++ Examples/05 Factory Demo/src/main/java/com/target/training/dao/impl/FileProductDao.java
@@ -0,0 +1,11 @@
+package com.target.training.dao.impl;
+
+import com.target.training.dao.DaoException;
+import com.target.training.dao.ProductDao;
+
+public class FileProductDao implements ProductDao {
+
+    public int count() throws DaoException {
+        return 0;
+    }
+}
