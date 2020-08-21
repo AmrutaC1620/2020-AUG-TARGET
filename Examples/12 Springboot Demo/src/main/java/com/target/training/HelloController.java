d38d0585174c638117773f799b38b8c542812b0a
Vinod Kumar Kayartaya <vinod@vinod.co>
2020-08-21 17:32:54 +0530

    🇮🇳

--- Examples/12 Springboot Demo/src/main/java/com/target/training/HelloController.java
+++ Examples/12 Springboot Demo/src/main/java/com/target/training/HelloController.java
@@ -1,15 +1,42 @@
 package com.target.training;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+
+// the url /api/hello is a resource, and has 2 representations
+// 1. plain text (MIME type: text/plain)
+// 2. json (MIME type: application/json)
 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
 
-    @RequestMapping("/hello")
+    // This function is called as "request handler"
+    // DispatcherServlet checks with HandlerMapping object for a client
+    // request called "/api/hello", and if found, will forward the request to
+    // this function
+    @GetMapping(value="/hello", produces = "text/plain")
     public String sayHello() {
         return "Hello, world!";
+    }
+
+    // @RequestMapping(value="/hello, produces="application/json", method=RequestMethod.GET)
+    @GetMapping(value="/hello", produces = "application/json")
+    public Map<String, Object> sayHelloAsJson() {
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("message", "Hello, World!");
+        map.put("from", "Vinod Kumar Kayartaya");
+        map.put("timestamp", new Date());
+
+        return map;
     }
 
     @RequestMapping("/welcome")
--- Examples/12 Springboot Demo/src/main/java/com/target/training/Main.java
+++ Examples/12 Springboot Demo/src/main/java/com/target/training/Main.java
@@ -2,17 +2,9 @@ package com.target.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class Main {
-
-    @RequestMapping("/")
-    public String index() {
-        return "This is the home of restful services";
-    }
 
     public static void main(String[] args) {
         // The following statement, starts up Apache tomcat, and uses Main.class as a
